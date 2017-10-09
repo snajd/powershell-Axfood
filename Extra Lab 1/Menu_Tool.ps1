a87568ce267fd424b50a1f58a5e1c8a85b96aae7
snajd <robin.engstrom@gmail.com>
2017-10-09 10:40:39 +0200

    Some changes

--- Extra Lab 1/Menu_Tool.ps1
+++ Extra Lab 1/Menu_Tool.ps1
@@ -6,32 +6,33 @@
 
 do 
 {
-    write-host -ForegroundColor green "1) Testa Aftonbladet"
-    write-host -ForegroundColor green "2) Testa DN"
-    write-host -ForegroundColor green "3) Testa Svenska Dagbladet"
-    write-host -ForegroundColor Yellow "Tryck "Q" för att avbryta" 
+    write-host "SITE TESTER TOOL"
+    write-host ""
+    write-host ""
+    write-host -ForegroundColor green "1) Test Aftonbladet"
+    write-host -ForegroundColor green "2) Test DN"
+    write-host -ForegroundColor green "3) Test Svenska Dagbladet"
+    write-host -ForegroundColor Yellow "Or ress (Q) to quit" 
      
-    $input = read-host "Vilken site vill du testa?"
+    $input = read-host "What site do you want to test?"
     switch ($input) {
         1 {$testsite = "www.aftonbladet.se"}
         2 {$testsite = "www.dn.se"}
         3 {$testsite = "www.svd.se"}
         }
-        write-host -ForegroundColor green "1) Testa Aftonbladet"
-        write-host -ForegroundColor green "2) Testa DN"
-        write-host -ForegroundColor green "3) Testa Svenska Dagbladet"
-        write-host -ForegroundColor Yellow "Tryck "Q" för att avbryta"
         
-        if (Test-NetConnection -port 80 -ComputerName $testsite) {
-            Clear-Host
-            write-host -ForegroundColor White "$testsite är uppe!"
-            write-host ""
-            pause
-        } else {
-            clear-host
-            write-host -ForeGroundColor Red "$testsite svarar inte"
-            pause
+        if ($input -ne "q") {
+            if (Test-NetConnection -port 80 -ComputerName $testsite) {
+                write-host -ForegroundColor White "$testsite is up!"
+                write-host ""
+                pause
+            } else {
+                write-host -ForeGroundColor Red "$testsite does'nt respond"
+                pause
+            }
         }
+
+
         
 
 } while ($input -ne "q")
