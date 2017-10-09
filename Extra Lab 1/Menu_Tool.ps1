6242e5037ce21ceb5ded9fa2ceedf4a86c486e4d
snajd <robin.engstrom@gmail.com>
2017-10-09 16:11:22 +0200

    Some cosmetic improvements

--- Extra Lab 1/Menu_Tool.ps1
+++ Extra Lab 1/Menu_Tool.ps1
@@ -6,12 +6,13 @@
 
 do 
 {
-    write-host "SITE TESTER TOOL"
-    write-host ""
-    write-host ""
+    write-host -BackgroundColor DarkRed -ForegroundColor Yellow "                                            "
+    write-host -BackgroundColor DarkRed -ForegroundColor Yellow " **** *** * * SITE TESTER TOOL * * *** **** "
+    write-host -BackgroundColor DarkRed -ForegroundColor Yellow "                                            "
     write-host -ForegroundColor green "1) Test Aftonbladet"
     write-host -ForegroundColor green "2) Test DN"
     write-host -ForegroundColor green "3) Test Svenska Dagbladet"
+    write-host ""
     write-host -ForegroundColor Yellow "Or ress (Q) to quit" 
      
     $input = read-host "What site do you want to test?"
